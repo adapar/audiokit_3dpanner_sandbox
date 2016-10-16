78ddfe1c9180b55a7ed0221ed5e526120806da37
Andrés Aparicio <adapar@gmail.com>
2016-10-16 12:52:37 -0300

    Initial Commit

---
+++ escondite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  escondite
+//
+//  Created by Andrés David Aparicio Alonso on 10/16/16.
+//  Copyright © 2016 cerebro de plastilina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
