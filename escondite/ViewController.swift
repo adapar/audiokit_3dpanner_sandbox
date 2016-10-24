be9a9a09cdd8fd656e4161621befc6927c5de295
Andrés Aparicio <adapar@gmail.com>
2016-10-24 17:40:20 -0300

    Testing placement

--- escondite/ViewController.swift
+++ escondite/ViewController.swift
@@ -11,14 +11,28 @@ import AudioKit
 
 class ViewController: UIViewController {
 
+    var currentX = 0.0
+    var currentY = 0.0
+    var currentZ = 0.0
+    
     var oscillator = AKOscillator()
     var panner: AK3DPanner?
+    
+    @IBOutlet weak var sliderX: UISlider!
+    @IBOutlet weak var labelX: UILabel!
+    
+    @IBOutlet weak var sliderY: UISlider!
+    @IBOutlet weak var labelY: UILabel!
+    
+    @IBOutlet weak var sliderZ: UISlider!
+    @IBOutlet weak var labelZ: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        panner = AK3DPanner(oscillator, x: 0, y: 0, z: 0)
+        panner = AK3DPanner(oscillator, x: currentX, y: currentY, z: currentZ)
+        
         AudioKit.output = panner
         AudioKit.start()
     }
@@ -28,6 +42,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func updateX(_ sender: AnyObject) {
+        currentX = Double(sliderX.value)
+        panner?.x = currentX
+        labelX.text = String.localizedStringWithFormat("%.1f", currentX)
+    }
+    
+    @IBAction func updateY(_ sender: AnyObject) {
+        currentY = Double(sliderY.value)
+        panner?.y = currentY
+        labelY.text = String.localizedStringWithFormat("%.1f", currentY)
+    }
+    
+    @IBAction func updateZ(_ sender: AnyObject) {
+        currentZ = Double(sliderZ.value)
+        panner?.z = currentZ
+        labelZ.text = String.localizedStringWithFormat("%.1f", currentZ)
+    }
+    
     @IBAction func toggleSound(_sender: UIButton) {
         if oscillator.isPlaying {
             oscillator.stop()
@@ -39,6 +71,5 @@ class ViewController: UIViewController {
             _sender.setTitle("Stop Sine Wave at \(Int(oscillator.frequency))Hz", for: .normal)
         }
     }
-
 }
 
