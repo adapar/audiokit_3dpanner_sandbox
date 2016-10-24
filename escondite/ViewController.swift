c9ac7323266b59965f8373bd814c84a0d092e090
Andrés Aparicio <adapar@gmail.com>
2016-10-24 17:01:41 -0300

    plays 440, 0,0,0

--- escondite/ViewController.swift
+++ escondite/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import AudioKit
 
 class ViewController: UIViewController {
 
+    var oscillator = AKOscillator()
+    var panner: AK3DPanner?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        panner = AK3DPanner(oscillator, x: 0, y: 0, z: 0)
+        AudioKit.output = panner
+        AudioKit.start()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func toggleSound(_sender: UIButton) {
+        if oscillator.isPlaying {
+            oscillator.stop()
+            _sender.setTitle("Play Sine Wave", for: .normal)
+        } else {
+            oscillator.amplitude = 1//random(0.5, 1)
+            oscillator.frequency = 440//random(220, 880)
+            oscillator.start()
+            _sender.setTitle("Stop Sine Wave at \(Int(oscillator.frequency))Hz", for: .normal)
+        }
+    }
 
 }
 
